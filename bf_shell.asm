2cd76705273f9f5a33a21cedaadaa43de0f0b01d
Daniel Joseph <danielomololu2002@gmail.com>
2026-03-07 15:49:42 +0100

    Update bf interpreter to use console library

--- bf_shell.asm
+++ bf_shell.asm
@@ -10,11 +10,30 @@ dd START
 dd I_END
 dd MEM
 dd STACKTOP
-dd parameters
+dd 00
 dd 00
 
-parameters rb 1024
+parameters rb 256
+include 'proc32.inc'
 include 'macros.inc'
+include 'dll.inc'
+include 'string.inc'
+
+align 16
+@IMPORT:
+library  terminal, 'console.obj'
+
+import terminal, \
+        con_init, 'con_init', \
+        con_exit, 'con_exit', \
+        con_gets, 'con_gets', \
+        con_write, 'con_write_asciiz', \
+        con_clear, 'con_cls', \
+        con_getc,  'con_getch', \
+        con_write_string, 'con_write_string', \
+        con_printf,  'con_printf'
+
+
 @pid dd 0
 include 'shell.inc'
 
@@ -29,13 +48,56 @@ tape rb TAPE_SIZE
 
 Ops rb BUFFER_SIZE / 2 ; use half of buffer size
 arg rd BUFFER_SIZE / 2 ; use half of buffer size
-
 prompt db "BF:> ", 0
+TITLE db "                       BRAINFUCK INTERPRETER  ",0
 err1 db "Missing parenthesis match",0
 err2 db "Invalid source file",0
+REPL db "in repl",10, 0
+exit_cmd db "exit", 0
+run_cmd  db "run", 0
+run_cmd_space db "run "
+clear_cmd db "clear", 0
+help_cmd  db "help", 0
+
+helps db "A Brainfuck Interpreter"
+
 ERR_SIZE = 36
 ERR2_SIZE = 30
-nl db 10
+nl db 10, 0
+
+macro write2 a {
+        local ..string, ..label
+        jmp   ..label
+..string db a, 0
+..label:
+        invoke con_write, ..string
+}
+
+macro write a {
+        local ..string, ..label
+        jmp   ..label
+..string db a, 10, 0
+..label:
+        invoke con_write, ..string
+}
+align 4
+
+file_info_struct:
+   .fn dd 0
+   .offset dd 0
+   .offset2 dd 0
+   .size dd 0
+   .ptr dd 0
+   .ecd db 0
+   .path dd 0
+
+macro DISPATCH {
+        movzx  eax, byte [ebp+ecx]
+        mov    ebx, ecx
+        inc    ecx
+        jmp    dword [jmp_table + eax *4]
+}
+
 
 jmp_table:
         dd exit_interpret
@@ -62,48 +124,105 @@ jmp_table:
            dd BEGIN
         }
 
-
 START:
-        call   shell.init
-        ; shps   "Starting..."
-        shln
-        ; shps  "Parameters: "
-        ; shpsa  parameters
-        cmp    byte[parameters], 0
-        jne    exit
+        mcall   68, 11
+        stdcall dll.Load, @IMPORT
+        or      eax, eax
+        jnz     EXIT
+        invoke  con_init, 80, 120, -1, -1, TITLE
 
 repl:
-        ; shps  "Repl"
-        shpsa  prompt
-        shgs   buffer_data
-        shpsa  buffer_data
-        call   interpret
-        shln
-        jmp    repl
+        invoke  con_write, prompt
+        invoke  con_gets, buffer_data, BUFFER_SIZE
+        stdcall string.length, buffer_data
+        mov     [buffer_data + eax -1], 0
 
-macro DISPATCH {
-        movzx  eax, byte [ebp+ecx]
-        mov    ebx, ecx
-        inc    ecx
-        jmp    dword [jmp_table + eax *4]
-}
+        stdcall string.cmp, buffer_data, exit_cmd, 5 ; check if exit, length 5 to make sure it is only exit
+        test    eax, eax
+        jz      exit 
 
+        stdcall string.cmp, buffer_data, clear_cmd, 6
+        test    eax, eax
+        jz      .clear 
+
+        stdcall string.cmp, buffer_data, run_cmd_space, 4
+        test    eax, eax
+        jz      .run_prog
+
+        stdcall  string.cmp, buffer_data, help_cmd, 5
+        test     eax, eax
+        jz       .display_help
+
+        call    interpret
+        invoke  con_write, nl
+        jmp     repl
+
+.clear:
+        invoke  con_clear 
+        jmp     repl
+
+.display_help:
+        invoke  con_write, helps
+        jmp     repl
+
+.run_prog:
+; assume command is in format run filepath
+        lea     eax, [buffer_data + 4]
+        stdcall string.copy, eax, parameters
+        mov     esi, parameters
+        ; first get_file size
+
+        mov     eax, 70
+        mov     dword [file_info_struct], 0x0
+        mov     dword [file_info_struct + 12], BUFFER_SIZE-1
+        mov     dword [file_info_struct + 16], buffer_data
+        mov     dword [file_info_struct + 21], parameters
+        mov     ebx, file_info_struct
+        mcall
+
+        test    eax, eax
+        jnz     .check_eof
+
+.process_buffer:
+        mov     [buffer_data + ebx], byte 0
+        call    interpret
+        jmp     repl
+
+.check_eof:
+        cmp     eax, 6
+        je      .process_buffer
+
+.run_error:
+        write2  "Error opening file: "
+        invoke  con_write, parameters
+        invoke  con_write, nl
+        jmp     repl
+        
+.not_run:
+        write   "Invalid run Command. Use run file_path"
+        jmp     repl
+
+        
 ; interpret  a brainfuck program
 interpret:
     ;ecx -> ip, edx -> dp, esi => bracket depth, edi -> tape
         ; shps  "In interpet"
+        write  "In Interpret function"
         call   preprocess
+
         cmp    eax, -1 
         je     .match_error
+
         xor    ecx, ecx
         xor    edx, edx
         xor    esi, esi
         mov    edi, tape
         mov    ebp, Ops
+
         DISPATCH
 
 .match_error:
-        shpsa  err1
+        write  "Match error found" 
         ret
 
 exit_interpret:
@@ -118,7 +237,7 @@ vinc:
 
 vdec:
         mov    eax, [arg + ebx * 4]
-        sub    [edi + edx], eax
+        sub    byte [edi + edx], al
         DISPATCH
 
 pinc:
@@ -138,20 +257,24 @@ pdec:
 
 
 output:
+        pusha
         mov    ebx, [arg + ebx *4]
-        mov    al, byte [edi + edx]
 .loop:
+        lea    eax, [edi + edx]
         test   ebx, ebx
         je     .dispatch
         dec    ebx
-        call   shell.print_char
+        invoke con_write_string, eax, 1
         jmp    .loop
 .dispatch:
+        popa
         DISPATCH
 
 input:
-        shgc
+        pusha
+        invoke  con_getc
         mov    [edi + edx], al
+        popa
         DISPATCH
 
 brac_left:
@@ -172,7 +295,7 @@ brac_right:
 preprocess:
 ; esi as ptr for buffer, ebx as buffer reference
 ; edi as pointer to Ops and arg
-        shps   "Preprocessing"
+        write "in preprocess"
         push   ebp
         mov    ebp, esp
         mov    ebx, buffer_data 
@@ -241,7 +364,8 @@ preprocess:
         ret
 
 exit: 
-        call   shell.destroy
+        invoke con_exit, 1
+EXIT:
         mov    eax, -1
         int    0x40
 I_END:
